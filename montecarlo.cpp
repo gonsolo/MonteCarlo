973dd7dfbc6dd2fa372a1928bac58f661e3b361b
Gon Solo <gonsolo@gmail.com>
2017-02-23 14:17:29 +0100

    Add Monte Carlo solution.

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -1,15 +1,34 @@
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
+#include <random>
 
 using namespace std;
 const char newline = '\n';
 
-auto f = [](float x) { return pow(x, 3); };
+auto f = [](float x) { return powf(x, 3); };
 auto antiderivative = [](float x) { return pow(x, 4) / 4; };
 
 float analytical(float a, float b) {
 	return antiderivative(b) - antiderivative(a);
+}
+
+float montecarlo(float a, float b) {
+
+	default_random_engine generator;
+	uniform_real_distribution<float> distribution(a, b);
+	auto rand = [&distribution, &generator]() { return distribution(generator); };
+
+	int resolution = 1000;
+	float result = 0;
+	for (int i = 0; i < resolution; ++i) {
+		auto x = rand();
+		auto y = f(x);
+		result += y;
+	}
+	result /= resolution;
+
+	return result;
 }
 
 int main()
@@ -18,6 +37,7 @@ int main()
 	float b = 1;
 
 	cout << "Analytical solution of x^3 from 0 to 1: " << analytical(a, b) << newline;
+	cout << "Monte Carlo solution: " << montecarlo(a, b) << newline;
 
 	return  EXIT_SUCCESS;
 }
